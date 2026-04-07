07dc0cbe3d7e188a1773980b7bb99eba1ba0814f
samri000 <samriaha2021@gmail.com>
2026-04-08 01:14:26 +0530

    message routes added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,7 +113,7 @@ export const updateProfile = async (req, res) => {
 
         const uploadResponse =await cloudinary.uploader.upload(profilePic)
 
-        const updatedUser = await User.findbyIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
             userId, 
             { profilePic: uploadResponse.secure_url }, 
             { new: true }
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -13,7 +13,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    sameSite: "lax", // CSRF attacks
     secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
