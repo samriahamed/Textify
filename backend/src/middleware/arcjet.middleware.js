b98de49f408aba32db8812fd34acad40a1844bbc
samri000 <samriaha2021@gmail.com>
2026-04-11 04:50:17 +0530

    chat container setup done

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -6,9 +6,9 @@ export const arcjetProtection = async (req, res, next) => {
     try {
 
         //✅ ADD THIS HERE (TOP)
-        if (ENV.NODE_ENV === "development") {
-            return next();
-        }
+        //if (ENV.NODE_ENV === "development") {
+        //    return next();
+        //}
         
         const decision = await aj.protect(req)
 
