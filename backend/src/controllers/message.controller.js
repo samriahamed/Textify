2d330dd8136d5264fec7f3093090e842aa7fadb9
samri000 <samriaha2021@gmail.com>
2026-04-14 01:22:05 +0530

    send message functionality implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+
 
 export const getAllContacts = async (req, res) => {
   try {
